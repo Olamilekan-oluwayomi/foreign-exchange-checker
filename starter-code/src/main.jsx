7a6f6b170b2b604a004974d2f32dc7cd23604589
Olamilekan-oluwayomi <ilesanmiolamilekan05@gmail.com>
2026-07-07 14:02:53 +0100

    Wire CurrencyProvider and FavoritesProvider into app root

--- starter-code/src/main.jsx
+++ starter-code/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { CurrencyProvider } from "./context/CurrencyContext.jsx";
+import { FavoritesProvider } from "./context/FavoritesContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <CurrencyProvider>
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
+    </CurrencyProvider>
   </StrictMode>,
-)
+);
